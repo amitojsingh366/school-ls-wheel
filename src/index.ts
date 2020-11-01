614e762eb33b0b9d5fe31b4a8f0637e65e05dc0e
Amitoj Singh <amitojsingh366@gmail.com>
2020-11-01 10:04:47 +0300

    yayameow

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+
+
 const app: Application = express();
+
 app.set("view engine", "ejs");
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.render("something");
+});
 
-res.render("something");
-
-
-})
-app.listen(12345,()=>{console.log("server running")})
+app.listen(12345, () => { console.log("server running") })
