633afa2281a05d6c04a06e046ef9241fee94a7a6
Amitoj Singh <amitojsingh366@gmail.com>
2020-11-01 15:45:37 +0300

    i changed a lot...

--- static/wheel.js
+++ static/wheel.js
@@ -8,7 +8,6 @@ const wheelColors = [
     "#009688",
     "#607d8b"
 ];
-const random = Math.floor(Math.random() * wheelColors.length);
 var padding = { top: 20, right: 40, bottom: 0, left: 0 },
     w = 500 - padding.left - padding.right,
     h = 500 - padding.top - padding.bottom,
@@ -21,13 +20,12 @@ var padding = { top: 20, right: 40, bottom: 0, left: 0 },
 
 
 var data = [
-    { "label": "smtg", "value": 1, "question": "meowwwaa" },
-    { "label": "smtg2", "value": 1, "question": "meowwwafasvv" },
-    { "label": "smtg3", "value": 1, "question": "meowwwaeevf" },
-    { "label": "smtg4", "value": 1, "question": "meowwwjknjk" },
-    { "label": "smtg5", "value": 1, "question": "meowwwjjks" },
-    { "label": "smtg6", "value": 1, "question": "meowww" },
-
+    { "label": "smtg", "value": 1, "task": "1" },
+    { "label": "smtg2", "value": 1, "task": "2" },
+    { "label": "smtg3", "value": 1, "task": "3" },
+    { "label": "smtg4", "value": 1, "task": "4" },
+    { "label": "smtg5", "value": 1, "task": "5" },
+    { "label": "smtg6", "value": 1, "task": "6" },
 ];
 
 
@@ -54,9 +52,10 @@ var arcs = vis.selectAll("g.slice")
     .append("g")
     .attr("class", "slice");
 
-
 arcs.append("path")
-    .attr("fill", function(d, i) { return wheelColors[i]; })
+    .attr("fill", function(d, i) {
+        return wheelColors[i];
+    })
     .attr("d", function(d) { return arc(d); });
 
 arcs.append("text").attr("transform", function(d) {
@@ -77,15 +76,9 @@ function spin(d) {
 
     container.on("click", null);
 
-    console.log("OldPick: " + oldpick.length, "Data length: " + data.length);
-    if (oldpick.length == data.length) {
-        console.log("done");
-        container.on("click", null);
-        return;
-    }
+
 
     var ps = 360 / data.length,
-        pieslice = Math.round(1440 / data.length),
         rng = Math.floor((Math.random() * 1440) + 360);
 
     rotation = (Math.round(rng / ps) * ps);
@@ -94,12 +87,6 @@ function spin(d) {
     picked = picked >= data.length ? (picked % data.length) : picked;
 
 
-    if (oldpick.indexOf(picked) !== -1) {
-        d3.select(this).call(spin);
-        return;
-    } else {
-        oldpick.push(picked);
-    }
 
     rotation += 90 - Math.round(ps / 2);
 
@@ -107,14 +94,9 @@ function spin(d) {
         .duration(3000)
         .attrTween("transform", rotTween)
         .each("end", function() {
-            d3.select(".slice:nth-child(" + (picked + 1) + ") path")
-                .attr("fill", "#111");
-
             d3.select("#question h1")
-                .text(data[picked].question);
-
+                .text(data[picked].task);
             oldrotation = rotation;
-
             container.on("click", spin);
         });
 }
@@ -123,20 +105,20 @@ svg.append("g")
     .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h / 2) + padding.top) + ")")
     .append("path")
     .attr("d", "M-" + (r * .15) + ",0L0," + (r * .05) + "L0,-" + (r * .05) + "Z")
-    .style({ "fill": "black" });
+    .style({ "fill": "white" });
 
 container.append("circle")
     .attr("cx", 0)
     .attr("cy", 0)
     .attr("r", 60)
-    .style({ "fill": "white", "cursor": "pointer" });
+    .style({ "fill": "#2C2F33", "cursor": "pointer" });
 
 container.append("text")
     .attr("x", 0)
     .attr("y", 15)
     .attr("text-anchor", "middle")
     .text("SPIN")
-    .style({ "font-weight": "bold", "font-size": "30px" });
+    .style({ "font-weight": "bold", "font-size": "30px", "fill": "white" });
 
 
 function rotTween(to) {
@@ -149,13 +131,11 @@ function rotTween(to) {
 
 function getRandomNumbers() {
     var array = new Uint16Array(1000);
-    var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
 
     if (window.hasOwnProperty("crypto") && typeof window.crypto.getRandomValues === "function") {
         window.crypto.getRandomValues(array);
         console.log("works");
     } else {
-        //no support for crypto, get crappy random numbers
         for (var i = 0; i < 1000; i++) {
             array[i] = Math.floor(Math.random() * 100000) + 1;
         }
