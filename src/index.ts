c86e2a6483a7a42cb06f67b260ea3b0d73195a93
Amitoj Singh <amitojsingh366@gmail.com>
2020-11-01 13:38:57 +0300

    ghdfbhdahewf

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
-
+import path from "path";
 
 const app: Application = express();
 
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
+app.use('/static', express.static(path.join(__dirname, '../static')));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.render("something");
+    res.render('wheel');
 });
 
 app.listen(12345, () => { console.log("server running") })
---
+++ static/wheel.js
@@ -0,0 +1,165 @@
+const wheelColors = [
+    "#ab47bc",
+    "#ef5350",
+    "#5c6bc0",
+    "#7e57c2",
+    "#42a5f5",
+    "#00bcd4",
+    "#009688",
+    "#607d8b",
+];
+const random = Math.floor(Math.random() * wheelColors.length);
+var padding = { top: 20, right: 40, bottom: 0, left: 0 },
+    w = 500 - padding.left - padding.right,
+    h = 500 - padding.top - padding.bottom,
+    r = Math.min(w, h) / 2,
+    rotation = 0,
+    oldrotation = 0,
+    picked = 100000,
+    oldpick = [];
+
+
+
+var data = [
+    { "label": "smtg", "value": 1, "question": "meowwwaa" },
+    { "label": "smtg2", "value": 1, "question": "meowwwafasvv" },
+    { "label": "smtg3", "value": 1, "question": "meowwwaeevf" },
+    { "label": "smtg4", "value": 1, "question": "meowwwjknjk" },
+    { "label": "smtg5", "value": 1, "question": "meowwwjjks" },
+    { "label": "smtg6", "value": 1, "question": "meowww" },
+
+];
+
+
+var svg = d3.select('#chart')
+    .append("svg")
+    .data([data])
+    .attr("width", w + padding.left + padding.right)
+    .attr("height", h + padding.top + padding.bottom);
+
+var container = svg.append("g")
+    .attr("class", "chartholder")
+    .attr("transform", "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")");
+
+var vis = container
+    .append("g");
+
+var pie = d3.layout.pie().sort(null).value(function(d) { return 1; });
+
+var arc = d3.svg.arc().outerRadius(r);
+
+var arcs = vis.selectAll("g.slice")
+    .data(pie)
+    .enter()
+    .append("g")
+    .attr("class", "slice");
+
+
+arcs.append("path")
+    .attr("fill", function(d, i) { return wheelColors[i]; })
+    .attr("d", function(d) { return arc(d); });
+
+arcs.append("text").attr("transform", function(d) {
+        d.innerRadius = 0;
+        d.outerRadius = r;
+        d.angle = (d.startAngle + d.endAngle) / 2;
+        return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius - 10) + ")";
+    })
+    .attr("text-anchor", "end")
+    .text(function(d, i) {
+        return data[i].label;
+    });
+
+container.on("click", spin);
+
+
+function spin(d) {
+
+    container.on("click", null);
+
+    console.log("OldPick: " + oldpick.length, "Data length: " + data.length);
+    if (oldpick.length == data.length) {
+        console.log("done");
+        container.on("click", null);
+        return;
+    }
+
+    var ps = 360 / data.length,
+        pieslice = Math.round(1440 / data.length),
+        rng = Math.floor((Math.random() * 1440) + 360);
+
+    rotation = (Math.round(rng / ps) * ps);
+
+    picked = Math.round(data.length - (rotation % 360) / ps);
+    picked = picked >= data.length ? (picked % data.length) : picked;
+
+
+    if (oldpick.indexOf(picked) !== -1) {
+        d3.select(this).call(spin);
+        return;
+    } else {
+        oldpick.push(picked);
+    }
+
+    rotation += 90 - Math.round(ps / 2);
+
+    vis.transition()
+        .duration(3000)
+        .attrTween("transform", rotTween)
+        .each("end", function() {
+            d3.select(".slice:nth-child(" + (picked + 1) + ") path")
+                .attr("fill", "#111");
+
+            d3.select("#question h1")
+                .text(data[picked].question);
+
+            oldrotation = rotation;
+
+            container.on("click", spin);
+        });
+}
+
+svg.append("g")
+    .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h / 2) + padding.top) + ")")
+    .append("path")
+    .attr("d", "M-" + (r * .15) + ",0L0," + (r * .05) + "L0,-" + (r * .05) + "Z")
+    .style({ "fill": "black" });
+
+container.append("circle")
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .attr("r", 60)
+    .style({ "fill": "white", "cursor": "pointer" });
+
+container.append("text")
+    .attr("x", 0)
+    .attr("y", 15)
+    .attr("text-anchor", "middle")
+    .text("SPIN")
+    .style({ "font-weight": "bold", "font-size": "30px" });
+
+
+function rotTween(to) {
+    var i = d3.interpolate(oldrotation % 360, rotation);
+    return function(t) {
+        return "rotate(" + i(t) + ")";
+    };
+}
+
+
+function getRandomNumbers() {
+    var array = new Uint16Array(1000);
+    var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
+
+    if (window.hasOwnProperty("crypto") && typeof window.crypto.getRandomValues === "function") {
+        window.crypto.getRandomValues(array);
+        console.log("works");
+    } else {
+        //no support for crypto, get crappy random numbers
+        for (var i = 0; i < 1000; i++) {
+            array[i] = Math.floor(Math.random() * 100000) + 1;
+        }
+    }
+
+    return array;
+}
