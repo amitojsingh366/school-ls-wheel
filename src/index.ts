2a20a5d6e333771e3ca64f052ba3867d1993b80c
Amitoj Singh <amitojsingh366@gmail.com>
2020-11-01 16:07:15 +0300

    changed index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import path from "path";
 import serveindex from "serve-index";
 
+const port = 12345;
 const app: Application = express();
 
 app.set("views", path.join(__dirname, "../views"));
@@ -13,6 +14,5 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use('/static', serveindex(path.join(__dirname, '../static')));
-//app.use('/static', express.static(path.join(__dirname, '../static')));
 
-app.listen(12345, () => { console.log("server running") })
+app.listen(port, () => { console.log(`Server Running on port ${port}\nYou can visit it here: http://localhost:${port}`) })
