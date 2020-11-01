ae7c207e8f4907879621f8da3aecc1f30f031a7a
Amitoj Singh <amitojsingh366@gmail.com>
2020-11-01 15:54:30 +0300

    i changed a lot...again.....

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import path from "path";
+import serveindex from "serve-index";
 
 const app: Application = express();
 
@@ -10,5 +11,8 @@ app.use('/static', express.static(path.join(__dirname, '../static')));
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.render('wheel');
 });
+
+app.use('/static', serveindex(path.join(__dirname, '../static')));
+//app.use('/static', express.static(path.join(__dirname, '../static')));
 
 app.listen(12345, () => { console.log("server running") })
